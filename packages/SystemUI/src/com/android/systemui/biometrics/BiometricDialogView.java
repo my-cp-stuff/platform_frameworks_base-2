AOSPA/android_frameworks_base-old

base: Use 2 modalities concurrently in BiometricPrompt if available
* Add a new dialog view to incorporate both Face and Fingerprint icons
* Example: Face and fingerprint

Signed-off-by: rituj <ritujbeniwal@gmail.com>
Change-Id: Idaf47f4fbf062a865f0c7a02fd495fdb7133578e

packages/SystemUI/src/com/android/systemui/biometrics/BiometricDialogView.java
@@ -108,6 +108,8 @@ public abstract class BiometricDialogView extends LinearLayout {
     private boolean mAnimatingAway;
     private boolean mWasForceRemoved;
     private boolean mSkipIntro;
+    private boolean mIsFingerprint;
+    private boolean mIsFace;
     protected boolean mRequireConfirmation;
     private int mUserId; // used to determine if we should show work background
 
@@ -346,6 +348,15 @@ public abstract class BiometricDialogView extends LinearLayout {
         mSkipIntro = false;
     }
 
+    protected int getAnimatingAwayDuration() {
+        return ANIMATION_DURATION_AWAY;
+    }
+
+    public void setFaceAndFingerprint(boolean isFingerprint, boolean isFace) {
+        mIsFace = isFace;
+        mIsFingerprint = isFingerprint;
+    }
+
     private void setDismissesDialog(View v) {
         v.setClickable(true);
         v.setOnClickListener(v1 -> {
@@ -382,13 +393,13 @@ public abstract class BiometricDialogView extends LinearLayout {
             public void run() {
                 mLayout.animate()
                         .alpha(0f)
-                        .setDuration(ANIMATION_DURATION_AWAY)
+                        .setDuration(getAnimatingAwayDuration())
                         .setInterpolator(mLinearOutSlowIn)
                         .withLayer()
                         .start();
                 mDialog.animate()
                         .translationY(mAnimationTranslationOffset)
-                        .setDuration(ANIMATION_DURATION_AWAY)
+                        .setDuration(getAnimatingAwayDuration())
                         .setInterpolator(mLinearOutSlowIn)
                         .withLayer()
                         .withEndAction(endActionRunnable)

my-cp-stuff/platform_frameworks_base-2

packages/SystemUI/src/com/android/systemui/biometrics/BiometricDialogView.java
@@ -114,6 +114,8 @@ public abstract class BiometricDialogView extends LinearLayout {
     private boolean mWasForceRemoved;
     private boolean mSkipIntro;
     protected boolean mAppLockDialog;
+    private boolean mIsFingerprint;
+    private boolean mIsFace;
     protected boolean mRequireConfirmation;
     private int mUserId; // used to determine if we should show work background
 
@@ -406,6 +408,14 @@ public abstract class BiometricDialogView extends LinearLayout {
         } else {
             return R.string.applock_fingerprint;
         }
+
+    protected int getAnimatingAwayDuration() {
+        return ANIMATION_DURATION_AWAY;
+    }
+
+    public void setFaceAndFingerprint(boolean isFace, boolean isFingerprint) {
+        mIsFace = isFace;
+        mIsFingerprint = isFingerprint;
     }
 
     private void setDismissesDialog(View v) {
@@ -444,13 +454,13 @@ public abstract class BiometricDialogView extends LinearLayout {
             public void run() {
                 mLayout.animate()
                         .alpha(0f)
-                        .setDuration(ANIMATION_DURATION_AWAY)
+                        .setDuration(getAnimatingAwayDuration())
                         .setInterpolator(mLinearOutSlowIn)
                         .withLayer()
                         .start();
                 mDialog.animate()
                         .translationY(mAnimationTranslationOffset)
-                        .setDuration(ANIMATION_DURATION_AWAY)
+                        .setDuration(getAnimatingAwayDuration())
                         .setInterpolator(mLinearOutSlowIn)
                         .withEndAction(endActionRunnable)
                         .start();